302d3ed07faa81dbee378d338562ef35c453687a
Shreyas Sathe <47695170+SerraTorr@users.noreply.github.com>
2020-06-20 17:53:15 +0530

    Update contact.php
    editted contact form

--- contact.php
+++ contact.php
@@ -281,39 +281,7 @@
                     </script>
 
                 </div>
-                <div class="row">
-                    <div class="col-12">
-                        <h2 class="contact-title">Get in Touch</h2>
-                    </div>
-                    <div class="col-lg-8">
-                        <form class="form-contact contact_form" action="contact_process.php" method="post" id="contactForm" novalidate="novalidate">
-                            <div class="row">
-                                <div class="col-12">
-                                    <div class="form-group">
-                                        <textarea class="form-control w-100" name="message" id="message" cols="30" rows="9" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter Message'" placeholder=" Enter Message"></textarea>
-                                    </div>
-                                </div>
-                                <div class="col-sm-6">
-                                    <div class="form-group">
-                                        <input class="form-control valid" name="name" id="name" type="text" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter your name'" placeholder="Enter your name">
-                                    </div>
-                                </div>
-                                <div class="col-sm-6">
-                                    <div class="form-group">
-                                        <input class="form-control valid" name="email" id="email" type="email" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter email address'" placeholder="Email">
-                                    </div>
-                                </div>
-                                <div class="col-12">
-                                    <div class="form-group">
-                                        <input class="form-control" name="subject" id="subject" type="text" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter Subject'" placeholder="Enter Subject">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="form-group mt-3">
-                                <button type="submit" class="button button-contactForm boxed-btn">Send</button>
-                            </div>
-                        </form>
-                    </div>
+                    <?php include 'contact-form.html';  ?>
                     <div class="col-lg-3 offset-lg-1">
                         <h5>Corporate Office:</h5><br>
                         <div class="media contact-info">
@@ -331,13 +299,13 @@
                                 <!-- <p>Mon to Fri 9am to 6pm</p> -->
                             </div>
                         </div>
-                        <h5>Factory Address:</h5><br>
-                        <div class="media contact-info">
-                            <span class="contact-info__icon"><i class="ti-home"></i></span>
-                            <div class="media-body">
-                                <p>Unit no. 7, Survey No. 57, H.No, 2, Goraipada, Vasai (East), District Palghar, Pincode – 401208</p>
-                            </div>
-                        </div>
+                        <!-- <h5>Factory Address:</h5><br> -->
+                        <!-- <div class="media contact-info"> -->
+                            <!-- <span class="contact-info__icon"><i class="ti-home"></i></span> -->
+                            <!-- <div class="media-body"> -->
+                                <!-- <p>Unit no. 7, Survey No. 57, H.No, 2, Goraipada, Vasai (East), District Palghar, Pincode – 401208</p> -->
+                            <!-- </div> -->
+                        <!-- </div> -->
                         <div class="media contact-info">
                             <span class="contact-info__icon"><i class="ti-tablet"></i></span>
                             <div class="media-body">
