5f2e9338a5e16276715efdcb8f4d79d782d10265
Shivam Chaturvedi <shivam4095@gmail.com>
2020-07-09 16:57:32 +0530

    Aur 'aesthetic change karne bola toh maa chod dunga'

--- header.php
+++ header.php
@@ -164,8 +164,8 @@
                                         <li>
                                             <div class="input-group" id="input" style="position:absolute;top:30%;left:30%;">
                                                 <form action="search-page.php" method="POST">
-                                                    <input style="position:absolute;right:35%;width:70%;top:2px;" type="text" name="search" class="form-control" placeholder="Search here.." onkeyup="searchq();" autocomplete="off">
-                                                  <button style="position:absolute;right:105%;height:40px;"class="btn btn-secondary" type="submit" name="submit">
+                                                    <input style="position:absolute;right:35%;width:65%;top:2px;" type="text" name="search" class="form-control" placeholder="Search here.." onkeyup="searchq();" autocomplete="off">
+                                                  <button style="position:absolute;right:100%;height:40px;"class="btn btn-secondary" type="submit" name="submit">
                                                     <i class="fa fa-search">
                                                     </i>
                                                   </button>
@@ -246,7 +246,7 @@
                             <ul>
                                 <li>
 
-                                    <div id="google_translate_element" style="position:absolute;right:10%;top:10%" class="d-none d-md-block"></div>
+                                    <div id="google_translate_element" style="position:absolute;right:13.5%;top:10%" class="d-none d-md-block"></div>
 
                                     <script type="text/javascript">
                                         function googleTranslateElementInit() {
